7cbb33dec53184315f0af535048cf959a83c16a6
Ramiz Perez <ramizzz13@gmail.com>
2021-09-22 11:02:00 -0500

    Variables en Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,20 @@
+const val PI = 3.1416
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
     // Try adding program arguments at Run/Debug configuration
     println("Program arguments: ${args.joinToString()}")
+
+    //Variable de lectura y escritura
+    var dinero = 10
+    println(dinero)
+    dinero = 5
+    println(dinero)
+
+    //Variable de solo lectura
+    val nombre = "Maria"
+    println(nombre)
+
+    println(PI)
 }
