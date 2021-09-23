961cf194ba89eca8662732004b194ca6c05b70be
Ramiz Perez <ramizzz13@gmail.com>
2021-09-23 18:57:43 -0500

    Also

---
+++ src/main/kotlin/Also.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    val moviles = mutableListOf("Google Pixel 2XL","Google Pixel 4a","Huawei Redmi 9","Xiaomi mi a3").also {
+        lista -> println("El valor original de la lista es $lista")
+    }.asReversed()
+    println(moviles)
+}
