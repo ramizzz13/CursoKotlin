e91d787ce9fa01935882c8a2ab9394cd04ec6260
Ramiz Perez <ramizzz13@gmail.com>
2021-09-23 18:41:42 -0500

    Run

---
+++ src/main/kotlin/Run.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val moviles = mutableListOf("Google Pixel 2XL","Google Pixel 4a","Huawei Redmi 9","Xiaomi mi a3")
+        .run {
+            removeIf { movil -> movil.contains("Google") }
+            this
+        }
+    println(moviles)
+
+}
