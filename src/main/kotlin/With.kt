b83a352990f6e9d3684c33a0afbbd974671a10e0
Ramiz Perez <ramizzz13@gmail.com>
2021-09-23 18:36:33 -0500

    With

---
+++ src/main/kotlin/With.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val colores = listOf("Azul", "Amarillo", "Rojo")
+    with(colores){
+        println("Nuestros colores son $this")
+        println("Esta lista tiene una cantidad de colores de $size")
+    }
+}
